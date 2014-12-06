7106c1053f1b2a1ee62d59b4ea093c63df8a5708
rebeccawilbur <rebeccawilbur@seattleacademy.org>
2014-12-06 12:10:10 -0800

    Reverted Threshold
    *facepalm*

--- Teliop/Teliop.c
+++ Teliop/Teliop.c
@@ -146,11 +146,11 @@ task main()
 
 
 //feed to motors/servos
-if(-10 < joystick.joy1_y1 < 10)
+if(-5 < joystick.joy1_y1 < 5)
 {
 	SpeedLeft = 0;
 }
-if(-10 < joystick.joy1_y2 < 10)
+if(-5 < joystick.joy1_y2 < 5)
 {
 	SpeedRight = 0;
 }
