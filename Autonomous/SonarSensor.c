4913799b1f07af5984f6ea29cc6d62aaaa56254a
Jesse Walling <jessephilipwalling@gmail.com>
2015-01-15 16:46:10 -0800

    Added basic sonar sensor code
    this is the beginning code for our sensors that will detect other robots
    in autonomous and stop.

---
+++ Autonomous/SonarSensor.c
@@ -0,0 +1,12 @@
+#pragma config(Sensor, S4, sonarSensor, sensorSONAR);
+//motor[motorA] = 100;
+//motor[motorB] = 100;
+int MotorSpeed = 0; // value of motor speed (0-100)
+loop {
+	if(SensorValue(sonarSensor) < 20) {
+		MotorSpeed = 0;
+	}
+	else {
+		return;
+	}
+}
