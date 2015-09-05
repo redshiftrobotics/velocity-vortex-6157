3cb0de9f86b2d50bc1d2df657d38ced500f4dd58
Alex Jordan <alex@strugee.net>
2015-09-05 15:18:49 -0700

    Reset for the 2015-2016 season

--- Autonomous/Position.c
+++
@@ -1,9 +0,0 @@
-#include "../Libraries/Motors.h"
-#include "../Libraries/Servos.h"
-#include ""
-
-float multiplier = 1.0; //This variable can account for different gear ratios between robots.
-
-
-
-/*			FORWARD			*/
--- Autonomous/ValueReader.c
+++
@@ -1,6 +0,0 @@
-#pragma config(Sensor, S2,     IROne,          sensorI2CCustom)
-#pragma config(Sensor, S3,     IRTwo,          sensorI2CCustom)
-#pragma config(Sensor, S1, , sensorI2CCustom)
-
-
-#include "JoystickDriver.c"
