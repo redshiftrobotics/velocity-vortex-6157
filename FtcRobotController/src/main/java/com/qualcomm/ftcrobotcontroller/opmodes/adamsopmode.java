ff73b13ac643ae5b48c53b562a75d4e4d6a63211
adamperlin <adamperlin@seattleacademy.org>
2015-11-07 15:14:36 -0800

    added support for front servos

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/adamsopmode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/adamsopmode.java
@@ -26,75 +26,93 @@ import com.qualcomm.robotcore.util.Range;
 public class adamsopmode extends OpMode {
 
 
-    //constructor
-public adamsopmode(){
+	//constructor
+	public adamsopmode() {
 
 
+	}
 
-}
-    private String startDate;
-    private ElapsedTime runtime = new ElapsedTime();
+	private String startDate;
+	private ElapsedTime runtime = new ElapsedTime();
 
 	//Controllers
-    private DcMotorController mydcmotorcontroller;
+	private DcMotorController mydcmotorcontroller;
+
 	private ServoController Robot_servo_Controller;
 	private DcMotorController myarmcontroller;
-
 	//Motors
-    private DcMotor my_dcmotor_left;
-    private DcMotor my_dcmotor_right;
+	private DcMotor my_dcmotor_left;
+	private DcMotor my_dcmotor_right;
 	private DcMotor dcmotor_arm;
 
 	//Servos
-	private Servo arm_servo;
+	//private Servo arm_servo;
 	private Servo robot_front_left;
 	private Servo robot_front_right;
 
-    /*
+	public double front_right_servo_position = 0;
+	public double front_left_servo_position = 0;
+
+	/*
      * Code to run when the op mode is first enabled goes here
      * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
      */
-    @Override
-    public void init() {
-        startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
-        runtime.reset();
+	@Override
+	public void init() {
+		startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+		runtime.reset();
 
-        mydcmotorcontroller = hardwareMap.dcMotorController.get("drive_controller");
+		mydcmotorcontroller = hardwareMap.dcMotorController.get("drive_controller");
 		myarmcontroller = hardwareMap.dcMotorController.get("arm_controller");
 		Robot_servo_Controller = hardwareMap.servoController.get("servo_controller");
+		dcmotor_arm = hardwareMap.dcMotor.get("arm_motor");
+		my_dcmotor_left = hardwareMap.dcMotor.get("left_drive");
+		my_dcmotor_right = hardwareMap.dcMotor.get("right_drive");
+		robot_front_left = hardwareMap.servo.get("frontleftservo");
+		robot_front_right = hardwareMap.servo.get("frontrightservo");
 
-		my_dcmotor_left= hardwareMap.dcMotor.get ("left_drive");
-       my_dcmotor_right = hardwareMap.dcMotor.get ("right_drive");
-
-		//dcmotor_arm = hardwareMap.dcMotor.get("arm_motor");
+		double front_right_servo_position = 0.0;
+		double front_left_servo_position = 0.0;
 
 
-    }
+	}
 
-    /*
+	/*
      * This method will be called repeatedly in a loop
      * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
      */
-    @Override
-    public void loop() {
-        telemetry.addData("1 Start", "NullOp started at " + startDate);
-        telemetry.addData("2 Status", "running for " + runtime.toString());
-
-
-        my_dcmotor_left.setPower(Range.clip((gamepad1.left_stick_y), -1, 1));
-        my_dcmotor_right.setPower(Range.clip ((-gamepad1.right_stick_y), -1, 1));
-
-
+	@Override
+	public void loop() {
+		telemetry.addData("1 Start", "NullOp started at " + startDate);
+		telemetry.addData("2 Status", "running for " + runtime.toString());
+		my_dcmotor_left.setPower(Range.clip((gamepad1.left_stick_y), -1, 1));
+		my_dcmotor_right.setPower(Range.clip((gamepad1.right_stick_y), -1, 1));
 		dcmotor_arm.setPower(Range.clip((-gamepad1.right_stick_x), -1, 1));
 
 
+		/*if (gamepad1.right_bumper && front_right_servo_position < 1.0) {
+
+			robot_front_right.setPosition(front_right_servo_position);
+			front_right_servo_position += 0.01;
+		}
+		if (gamepad1.left_bumper && front_left_servo_position < 1.0) {
+			robot_front_left.setPosition(front_left_servo_position);
+			front_left_servo_position+= 0.01;
+		}*/
+		if (gamepad1.a) {
+			robot_front_left.setPosition(1.0);
+
+		}
+		else if (gamepad1.b){robot_front_left.setPosition(0.0);}
+		if (gamepad1.x){
+
+			robot_front_right.setPosition(0.2);
+		}
+		else if (gamepad1.y){
+			robot_front_right.setPosition(1.0);
+		}
 
 
-    }
 
-
-
-
-
-
+	}
 }
