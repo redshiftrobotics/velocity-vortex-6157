15bab008b82b86b81afd7fbec82a359bcbda5fb3
jpwall <jessephilipwalling@gmail.com>
2015-11-27 14:12:43 -0800

    fix typo in autonomous

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/autonomous.java
@@ -66,7 +66,7 @@ public class autonomous extends LinearOpMode {
 
 
 		while(Math.abs(op.my_dcmotor_left.getCurrentPosition()) - startPos <= rotations*1400) {
-			op.my_dcmotor_left.setPower(1.0s);
+			op.my_dcmotor_left.setPower(1.0);
 			op.my_dcmotor_right.setPower(1.0);
 		}
 	}
